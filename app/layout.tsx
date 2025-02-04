4f121c0713370e0d05491a0ed67289850510ba7e
rebi13 <rebi13@naver.com>
2025-02-04 22:59:58 +0900

    Feature: metadata 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,15 @@
+'use client';
+
 import '@mantine/core/styles.css';
 
 import React from 'react';
-import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
-import { theme } from '../theme';
+import { ColorSchemeScript, Flex, mantineHtmlProps, MantineProvider } from '@mantine/core';
+import { ModalStackManager } from '@/components/Modal';
+import { theme } from '@/theme';
 
-export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
-};
-
-export default function RootLayout({ children }: { children: any }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
+    <html lang="ko" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/favicon.svg" />
@@ -19,9 +17,32 @@ export default function RootLayout({ children }: { children: any }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <style>
+          {`
+            html, body {
+              height: 100%;
+              margin: 0;
+              padding: 0;
+            }
+          `}
+        </style>
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme}>
+          <ModalStackManager>
+            <Flex
+              w="100%"
+              h="100vh"
+              miw="20rem"
+              maw="40rem"
+              mx="auto"
+              direction="column"
+              align="center"
+            >
+              {children}
+            </Flex>
+          </ModalStackManager>
+        </MantineProvider>
       </body>
     </html>
   );
---
+++ app/metadata.ts
@@ -0,0 +1,4 @@
+export const metadata = {
+  title: 'Your Wedding',
+  description: '너의 결혼을 축하하고 축복해',
+};
