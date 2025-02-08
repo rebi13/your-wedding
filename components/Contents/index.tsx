b43e21106518a0d6b33df017454af2c7ab0c0e01
rebi13 <rebi13@naver.com>
2025-02-08 23:43:27 +0900

    Feature: framer-motion 스크롤 애니메이션 적용

--- components/Contents/index.tsx
+++ components/Contents/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { motion } from 'framer-motion';
 import { Flex, Text, Title } from '@mantine/core';
 
 interface ContentsProps {
@@ -9,17 +10,28 @@ interface ContentsProps {
 
 export const Contents = ({ title, contents }: ContentsProps) => {
   return (
-    <Flex direction="column" p="md" align="center" gap="md">
-      {title && <Title order={2}>{title}</Title>}
-      <Flex direction="column" gap="xs">
-        {contents.map((content, index) => {
-          return (
-            <Text key={content + index} ta="center">
-              {content || '\u00A0'}
-            </Text>
-          );
-        })}
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false }}
+      transition={{
+        ease: 'easeInOut',
+        duration: 2,
+        y: { duration: 1 },
+      }}
+    >
+      <Flex direction="column" p="md" align="center" gap="md">
+        {title && <Title order={2}>{title}</Title>}
+        <Flex direction="column" gap="xs">
+          {contents.map((content, index) => {
+            return (
+              <Text key={content + index} ta="center">
+                {content || '\u00A0'}
+              </Text>
+            );
+          })}
+        </Flex>
       </Flex>
-    </Flex>
+    </motion.div>
   );
 };
