8d50585b5fc7e54809a3b329bc3c4ad15a52acec
rebi13 <rebi13@naver.com>
2025-02-09 17:49:13 +0900

    Feature: 복사 로직 적용

--- components/GiftAccount/index.tsx
+++ components/GiftAccount/index.tsx
@@ -1,4 +1,5 @@
 import { Accordion, Button, Flex, Text } from '@mantine/core';
+import { copyToClipboard } from '@/utils/copyData';
 import { FramerMotionWrapper } from '../FramerMotionWrapper';
 
 const groomSideInfo = [
@@ -27,18 +28,41 @@ export const GiftAccount = () => {
                 <Flex justify="space-between" align="center">
                   <Flex direction="column">
                     <Text>신랑</Text>
-                    <Text>01012345678</Text>
-                    <Text>기업은행 송재인</Text>
+                    <Text>01011112222</Text>
+                    <Text>기업은행 강건우</Text>
                   </Flex>
-                  <Button>복사</Button>
+                  <Button
+                    bg="orange"
+                    onClick={() => copyToClipboard('01011112222 기업은행 강건우')}
+                  >
+                    복사
+                  </Button>
                 </Flex>
                 <Flex justify="space-between" align="center">
                   <Flex direction="column">
-                    <Text>신랑</Text>
-                    <Text>01012345678</Text>
-                    <Text>기업은행 송재인</Text>
+                    <Text>신랑 아버지</Text>
+                    <Text>01022223333</Text>
+                    <Text>우리은행 강명성</Text>
                   </Flex>
-                  <Button>복사</Button>
+                  <Button
+                    bg="orange"
+                    onClick={() => copyToClipboard('01022223333 우리은행 강명성')}
+                  >
+                    복사
+                  </Button>
+                </Flex>
+                <Flex justify="space-between" align="center">
+                  <Flex direction="column">
+                    <Text>신랑 어머니</Text>
+                    <Text>01033334444</Text>
+                    <Text>국민은행 이수정</Text>
+                  </Flex>
+                  <Button
+                    bg="orange"
+                    onClick={() => copyToClipboard('01033334444 국민은행 이수정')}
+                  >
+                    복사
+                  </Button>
                 </Flex>
               </Flex>
             </Accordion.Panel>
@@ -54,18 +78,41 @@ export const GiftAccount = () => {
                 <Flex justify="space-between" align="center">
                   <Flex direction="column">
                     <Text>신부</Text>
-                    <Text>01087654321</Text>
-                    <Text>기업은행 최하슬</Text>
+                    <Text>01044445555</Text>
+                    <Text>카카오뱅크 하유리</Text>
                   </Flex>
-                  <Button>복사</Button>
+                  <Button
+                    bg="orange"
+                    onClick={() => copyToClipboard('01044445555 카카오뱅크 하유리')}
+                  >
+                    복사
+                  </Button>
                 </Flex>
                 <Flex justify="space-between" align="center">
                   <Flex direction="column">
-                    <Text>신부</Text>
-                    <Text>01087654321</Text>
-                    <Text>기업은행 최하슬</Text>
+                    <Text>신부 아버지</Text>
+                    <Text>01055556666</Text>
+                    <Text>신한은행 하주석</Text>
                   </Flex>
-                  <Button>복사</Button>
+                  <Button
+                    bg="orange"
+                    onClick={() => copyToClipboard('01055556666 신한은행 하주석')}
+                  >
+                    복사
+                  </Button>
+                </Flex>
+                <Flex justify="space-between" align="center">
+                  <Flex direction="column">
+                    <Text>신부 어머니</Text>
+                    <Text>01066667777</Text>
+                    <Text>케이뱅크 김소영</Text>
+                  </Flex>
+                  <Button
+                    bg="orange"
+                    onClick={() => copyToClipboard('01066667777 케이뱅크 김소영')}
+                  >
+                    복사
+                  </Button>
                 </Flex>
               </Flex>
             </Accordion.Panel>
