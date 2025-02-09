d969cdcf25fd181a68dcd3b39c754206872a5d0a
rebi13 <rebi13@naver.com>
2025-02-09 21:12:29 +0900

    Design: Title 컴포넌트 Text 컴포넌트로 변경 -> 폰트 적용

--- components/GuestBook/index.tsx
+++ components/GuestBook/index.tsx
@@ -1,20 +1,20 @@
 'use client';
 
-import { IconDotsVertical } from '@tabler/icons-react';
-import { ActionIcon, Card, Flex, Text, Title } from '@mantine/core';
+import { IconDotsVertical, IconEdit, IconTrash } from '@tabler/icons-react';
+import { ActionIcon, Card, Flex, Menu, Text } from '@mantine/core';
 import { FramerMotionWrapper } from '../FramerMotionWrapper';
 
 export const GuestBook = () => {
   return (
     <FramerMotionWrapper>
       <Flex direction="column" p="md" gap="md" w="100%" align="center">
-        <Title order={3}>방명록</Title>
+        <Text fz="2rem">방명록</Text>
         <Card
           shadow="sm"
           padding="md"
           radius="md"
           bg="#D7C8C2"
-          h="8rem"
+          mih="10rem"
           w="100%"
           style={{ gap: '1rem' }}
         >
@@ -23,11 +23,20 @@ export const GuestBook = () => {
               <Text fz="lg">은비</Text>
               <Text fz="xs">2025.02.06 03:22</Text>
             </Flex>
-            <ActionIcon variant="subtle" color="dark" size="sm">
-              <IconDotsVertical />
-            </ActionIcon>
+            <Menu shadow="md" width={80}>
+              <Menu.Target>
+                <ActionIcon variant="subtle" color="dark" size="sm">
+                  <IconDotsVertical />
+                </ActionIcon>
+              </Menu.Target>
+
+              <Menu.Dropdown>
+                <Menu.Item leftSection={<IconEdit size={14} />}>수정</Menu.Item>
+                <Menu.Item leftSection={<IconTrash size={14} />}>삭제</Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
           </Flex>
-          <Text>결혼 너무너무 축하해 행복하게 잘 살아!! 🥹🎉🎉🎉🎉</Text>
+          <Text lineClamp={3}>결혼 너무너무 축하해 행복하게 잘 살아!! 🥹🎉🎉🎉🎉</Text>
         </Card>
       </Flex>
     </FramerMotionWrapper>
