0528f67da7a9fb340f9f0d042eb89faa9afd263f
rebi13 <rebi13@naver.com>
2025-02-13 23:45:31 +0900

    Chore: 환경변수명 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,11 +43,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Script
           strategy="afterInteractive"
-          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_MAP_CLIENT_ID}`}
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_MAP_CLIENT_ID}`}
         />
         <Script
           strategy="afterInteractive"
-          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_MAP_CLIENT_ID}=geocoder`}
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_MAP_CLIENT_ID}=geocoder`}
         />
         <Template>{children}</Template>
       </body>
