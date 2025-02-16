9e719010edfb855b59b1f621e1cb917a72b9af52
rebi13 <rebi13@naver.com>
2025-02-16 23:35:17 +0900

    Chore: 환경변수명 변경

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -9,8 +9,8 @@ export const createServerSideClient = async (isServerComponent = false) => {
   const cookieStore = await cookies(); // key value store
 
   return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_SUPABASE_URL!,
+    process.env.NEXT_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get: (key) => cookieStore.get(key)?.value,
@@ -36,8 +36,8 @@ export const createServerSideClientRSC = async () => {
 // Middleware
 export const createServerSideClientMiddleware = async (req: NextRequest, res: NextResponse) => {
   return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_SUPABASE_URL!,
+    process.env.NEXT_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get: (key) => getCookie(key, { req, res }),
