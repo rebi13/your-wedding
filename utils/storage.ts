fb59057741ac5a2e6b1fb5bbcb10359270e51e08
rebi13 <rebi13@naver.com>
2025-03-02 00:49:57 +0900

    Chore: 주석 제거

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,4 +1,3 @@
 export const getImageUrl = (path: string) => {
   return `${process.env.NEXT_PUBLIC_SUPABASE_URL!}/storage/v1/object/public/${process.env.NEXT_PUBLIC_STORAGE_BUCKET!}/${path}`;
 };
-// https://fpvshxdloaswkodxjdlf.supabase.co/storage/v1/object/public/your-wedding//10.jpg
