c32fc3dc96d89835df82345ed8850a39ad207bab
rebi13 <rebi13@naver.com>
2025-03-02 21:19:22 +0900

    Chore: 주석 변경 (설명)

--- actions/image.actions.ts
+++ actions/image.actions.ts
@@ -2,7 +2,7 @@
 
 import { createServerSideClient } from '@/lib/supabase';
 
-// GuestBook List 가져오기
+// Image List 가져오기
 export const getImages = async () => {
   const supabase = await createServerSideClient();
   const { data } = await supabase.storage.from(process.env.NEXT_PUBLIC_STORAGE_BUCKET!).list();
