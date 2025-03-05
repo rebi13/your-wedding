851a8b6f24fa6a639e0baee30740007eb79e08cc
rebi13 <rebi13@naver.com>
2025-03-05 23:54:49 +0900

    Feature: 슬라이드 이동 시마다 인덱스 변경

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IconArrowDown, IconArrowLeft, IconArrowRight } from '@tabler/icons-react';
 import { Carousel } from '@mantine/carousel';
 import { Button, Flex, Image, Modal, SimpleGrid, Text } from '@mantine/core';
@@ -8,6 +8,7 @@ import { useDisclosure } from '@mantine/hooks';
 import useTotalController from '@/hooks/useTotalController';
 import { getImageUrl } from '@/utils/storage';
 import { FramerMotionWrapper } from '../FramerMotionWrapper';
+import classes from './Gallery.module.css';
 
 export const Gallery = () => {
   const { getWeddingImages } = useTotalController();
@@ -15,6 +16,10 @@ export const Gallery = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [opened, { open, close }] = useDisclosure(false);
   const { data: imageDatas } = getWeddingImages();
+
+  const totalImages = imageDatas?.length || 0;
+  const isFirstSlide = currentImageIndex === 0;
+  const isLastSlide = currentImageIndex === totalImages - 1;
 
   const openModalCarousel = (index: number) => {
     setCurrentImageIndex(index);
@@ -25,6 +30,12 @@ export const Gallery = () => {
     setLimit((prevLimit) => prevLimit + 9);
   };
 
+  useEffect(() => {
+    if (currentImageIndex >= limit) {
+      handleLoadMore();
+    }
+  }, [currentImageIndex]);
+
   return (
     <FramerMotionWrapper>
       <Flex direction="column" p="sm" gap="md" align="center">
@@ -32,6 +43,7 @@ export const Gallery = () => {
           갤러리
         </Text>
         <Text>사진을 클릭하시면 전체 화면 보기가 가능합니다.</Text>
+
         <Modal
           opened={opened}
           onClose={close}
@@ -40,44 +52,44 @@ export const Gallery = () => {
           transitionProps={{ transition: 'fade', duration: 200 }}
         >
           <Carousel
+            classNames={classes}
             height="100%"
-            nextControlIcon={<IconArrowRight size={16} />}
-            previousControlIcon={<IconArrowLeft size={16} />}
             initialSlide={currentImageIndex}
+            onSlideChange={setCurrentImageIndex}
+            nextControlIcon={!isLastSlide ? <IconArrowRight size={16} /> : null}
+            previousControlIcon={!isFirstSlide ? <IconArrowLeft size={16} /> : null}
           >
-            {imageDatas?.map((image) => {
-              return (
-                <Image
-                  key={image.id}
-                  alt="image"
-                  flex={1}
-                  src={getImageUrl(image.name)}
-                  style={{ objectFit: 'cover' }}
-                />
-              );
-            })}
+            {imageDatas?.map((image) => (
+              <Image
+                key={image.id}
+                alt="image"
+                flex={1}
+                src={getImageUrl(image.name)}
+                style={{ objectFit: 'cover' }}
+              />
+            ))}
           </Carousel>
         </Modal>
+
         <SimpleGrid cols={3}>
-          {imageDatas?.slice(0, limit).map((image, index) => {
-            return (
-              <Flex
-                key={image.id}
-                pos="relative"
-                w="6rem"
-                h="6rem"
-                onClick={() => openModalCarousel(index)}
-              >
-                <Image
-                  alt="image"
-                  flex={1}
-                  src={getImageUrl(image.name)}
-                  style={{ objectFit: 'cover' }}
-                />
-              </Flex>
-            );
-          })}
+          {imageDatas?.slice(0, limit).map((image, index) => (
+            <Flex
+              key={image.id}
+              pos="relative"
+              w="6rem"
+              h="6rem"
+              onClick={() => openModalCarousel(index)}
+            >
+              <Image
+                alt="image"
+                flex={1}
+                src={getImageUrl(image.name)}
+                style={{ objectFit: 'cover' }}
+              />
+            </Flex>
+          ))}
         </SimpleGrid>
+
         {imageDatas && imageDatas.length > limit && (
           <Button
             variant="subtle"
