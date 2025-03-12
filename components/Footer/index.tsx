e36b62e6ecd06e5de413571d0b8f63fcda2bc965
rebi13 <rebi13@naver.com>
2025-03-12 23:39:51 +0900

    Feature: 청첩장 공유 로직 추가

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,13 +1,34 @@
-import { Flex, Text } from '@mantine/core';
+import { Button, Flex, Text } from '@mantine/core';
 import useTotalController from '@/hooks/useTotalController';
+import { shareLink } from '@/utils/shareLink';
 
 export const Footer = () => {
   const { totalData: data } = useTotalController();
-  // TODO: DB에 json 데이터 넣기
+
+  const handleShare = async () => {
+    try {
+      await shareLink({
+        title: document.title,
+        text: '청첩장을 공유합니다.',
+        url: window.location.href,
+      });
+    } catch (error) {
+      // 타입 단언을 통해 에러 메시지 처리
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      } else {
+        // `error`가 객체 또는 문자열일 수 있음
+        throw new Error(String(error));
+      }
+    }
+  };
   return (
     <Flex direction="column" p="md" gap="xs" w="100%" align="center" mb="md">
       <Text>For Your Wedding</Text>
       <Text>{data?.madeBy}</Text>
+      <Button color="orange" variant="subtle" onClick={() => handleShare()}>
+        청첩장 공유하기
+      </Button>
     </Flex>
   );
 };
