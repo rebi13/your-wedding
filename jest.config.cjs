cfbf1458773cbd9e8636e1981122875d153daffa
rebi13 <rebi13@naver.com>
2025-03-15 16:26:07 +0900

    Test: 테스트코드 환경 세팅

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,6 +9,10 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/lib/(.*)$': '<rootDir>/lib/$1', // ✅ 추가
+    '^@/actions/(.*)$': '<rootDir>/actions/$1', // ✅ 추가
+    '^@/types/(.*)$': '<rootDir>/types/$1', // ✅ 추가
+    '^@/utils/(.*)$': '<rootDir>/utils/$1', // ✅ 추가
   },
   testEnvironment: 'jest-environment-jsdom',
 };
