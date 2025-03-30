4b452e73389caa4d9401350a0996875d4931dcf4
rebi13 <rebi13@naver.com>
2025-03-30 19:20:21 +0900

    Design: 배치 변경

--- components/Family/index.tsx
+++ components/Family/index.tsx
@@ -9,37 +9,58 @@ import { getImageUrl } from '@/utils/storage';
 export const Family = () => {
   const { totalData: data } = useTotalController();
 
-  const groomParents = data?.greeting?.host?.groom.parents.map((d) => d.name).join(' ・ ');
-  const brideParents = data?.greeting?.host?.bride.parents.map((d) => d.name).join(' ・ ');
+  // const groomParents = data?.greeting?.host?.groom.parents.map((d) => d.name).join(' ・ ');
+  // const brideParents = data?.greeting?.host?.bride.parents.map((d) => d.name).join(' ・ ');
+
+  const [groomDad, groomMom] = data?.greeting?.host?.groom.parents ?? [];
+  const [brideDad, brideMom] = data?.greeting?.host?.bride.parents ?? [];
 
   return (
     <FramerMotionWrapper>
       <Image src={getImageUrl('middle.png')} />
 
-      <Flex direction="column" w="100%" p="lg" px="6rem" bg="#D7C8C2" gap="md">
+      <Flex direction="column" w="100%" p="lg" px="4rem" bg="#D7C8C2" gap="md">
         <Flex direction="column" gap="xs">
-          <Flex justify="space-between">
-            <Text size="lg">{groomParents}</Text>
-            <Text size="lg">의 {data?.greeting.host.groom.relation}</Text>
-          </Flex>
-          <Flex justify="space-between">
-            <Text size="lg">{data?.greeting.host.groom.host}</Text>
-            <Text size="lg" fw={700}>
-              {data?.greeting.host.groom.name}
-            </Text>
+          <Flex justify="space-between" align="center">
+            <Flex align="center" gap="xs">
+              <Flex direction="column">
+                <Text fw="bold" size="lg">
+                  {groomDad.name}
+                </Text>
+                <Text fw="bold" size="lg">
+                  {groomMom.name}
+                </Text>
+              </Flex>
+              <Text>의</Text>
+            </Flex>
+            <Flex align="center" gap="xs">
+              <Text>{data?.greeting.host.groom.relation}</Text>
+              <Text fw="bold" size="lg">
+                {data?.greeting.host.groom.name}
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
         <Divider />
-        <Flex direction="column">
-          <Flex justify="space-between">
-            <Text size="lg">{brideParents}</Text>
-            <Text size="lg">의 {data?.greeting.host.bride.relation}</Text>
-          </Flex>
-          <Flex justify="space-between">
-            <Text size="lg">{data?.greeting.host.bride.host}</Text>
-            <Text size="lg" fw={700}>
-              {data?.greeting.host.bride.name}
-            </Text>
+        <Flex direction="column" gap="xs">
+          <Flex justify="space-between" align="center">
+            <Flex align="center" gap="xs">
+              <Flex direction="column">
+                <Text fw="bold" size="lg">
+                  {brideDad.name}
+                </Text>
+                <Text fw="bold" size="lg">
+                  {brideMom.name}
+                </Text>
+              </Flex>
+              <Text>의</Text>
+            </Flex>
+            <Flex align="center" gap="xs">
+              <Text>{data?.greeting.host.bride.relation}</Text>
+              <Text fw="bold" size="lg">
+                {data?.greeting.host.bride.name}
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
