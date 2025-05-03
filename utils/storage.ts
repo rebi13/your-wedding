246820d12c52c1ec8476a20feee08d48fc16076d
rebi13 <rebi13@naver.com>
2025-05-03 12:40:46 +0900

    Fix: 이미지 경로 수정

--- utils/storage.ts
+++ utils/storage.ts
@@ -3,5 +3,5 @@
 // };
 
 export const getImageUrl = (path: string, width = 480, quality = 80) => {
-  return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/render/image/public/${process.env.NEXT_PUBLIC_STORAGE_BUCKET}/${path}?width=${width}&quality=${quality}`;
+  return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${process.env.NEXT_PUBLIC_STORAGE_BUCKET}/${path}?width=${width}&quality=${quality}`;
 };
